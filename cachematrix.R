8463bea939b03d094be4da0933a0688a80541764
bmellinger11 <bmellinger@hotmail.com>
2015-05-22 16:09:30 -0700

    add interior comments
    Updated assignment file with additional commenting for interior
    functions (no change to code).

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,31 @@
-## This script contains functions for caching and retreiving the values of a
+## This script contains functions for caching and retrieving the values of a
 ## stored matrix as well as caching and retreiving the inverse of a stored
 ## invertible square matrix.
 
-## This first function contains storage and retreival functions for the
-## matrix to be inverted, as well as the associated inverted matrix
+## This first function contains storage and retrieval functions for the
+## matrix to be inverted, as well as the associated inverted matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+
+    ## clear the inverse matrix cache by default
     mx.inv <- NULL
+
+    ## sub-function for storing a matrix and clearing cache for inverse matrix
     setmx <- function(y) {
         mx <<- y
         mx.inv <<- NULL
     }
+
+    ## sub-function for retrieving the matrix from cache
     getmx <- function() mx
+
+    ## sub-function for storing the inverse matrix
     setinv <- function(solve) mx.inv <<- solve
+
+    ## sub-function for retrieving the inverse matrix
     getinv <- function() mx.inv
+
+    ## list of sub-functions that may be called from this function
     list(setmx = setmx, getmx = getmx,
          setinv = setinv, getinv = getinv)
 }
@@ -21,19 +33,34 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## This second function calculates the inverse matrix only if provided an
 ## invertible square matrix when the matrix has been stored previously
-## if previously calculated, the function will retreive the inverted matrix
-## from cached values instead of recalculating
+## if previously calculated, the function will retrieve the inverted matrix
+## from cached values instead of recalculating.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    ## attempt to retrieve the inverse matrix from cache
     mx.inv <- x$getinv()
+
+    ## determine if inverse matrix was successfully retrieved from cache
     if(!is.null(mx.inv)) {
         message("getting cached data...")
+
+        ## Return a matrix that is the inverse of 'x'
         return(mx.inv)
+
+    ## otherwise, calculate the inverse matrix and store the value in cache
     } else {
+
+        ## retrieve the matrix from cache
         mx <- x$getmx()
+
+        ## calculate the inverse matrix
         mx.inv <- solve(mx)
+
+        ## store the inverse matrix in cache
         x$setinv(mx.inv)
+
+        ## Return a matrix that is the inverse of 'x'
         mx.inv
     }
 }
--- test.cachematrix.R
+++ test.cachematrix.R
@@ -10,25 +10,34 @@
 source("cachematrix.R")
 
 ## 1.  builds the test matrix and shows default solve() function
+message("generating test matrix...")
 test <- c(9, 1, 3, 6, 13, 11, 7, 0, 5, 7, 4, 7, 2, 6, 1, 10)
-print(test)
 test.mx <- matrix(test,4,4)
 print(test.mx)
-solve(test.mx)
+message("checking to see what inverse should look like...")
+test.solve <- solve(test.mx)
+print(test.solve)
 
 ## 2. passes test matrix to into test object
 ##    (from - http://en.wikipedia.org/wiki/Square_matrix)
 ##    along with atomic caching functions
 ##    shows that matrix is cached after $setmx
 ##    is explicitly called on test matrix
+message("passing test matrix to makeCacheMatrix...")
 test.functs <- makeCacheMatrix(test.mx)
 test.functs$setmx(test.mx)
-test.functs$getmx()
+message("and checking if it can retrieve matrix from cache...")
+test.mxret <- test.functs$getmx()
+print(test.mxret)
+
 
 ## 3. tests cacheSolve function with test object
 ##    should return the inverted matrix of the test matrix
 ##    then should return the same matrix values from cache
+message("solving for inverse matrix and storing values...")
 cacheSolve(test.functs)
+message("and checking to see if inverse matrix is stored in cache...")
+message("")
 cacheSolve(test.functs)
 
 ## 4. passes back the cached inverted matrix to the $setmx, $getmx,
@@ -38,12 +47,12 @@ test.inv <- test.functs$getinv()
 print(test.inv)
 test.functs$setmx(test.inv)
 test.inv2 <- cacheSolve(test.functs)
-print("comparing reinverted matrix with original...")
+message("comparing reinverted matrix with original...")
 print(test.inv2)
 print(test.mx)
 if(test.inv2[4,4] == test.mx[4,4]) print("Success") else print("Unsuccessful")
-print(" Note: Values vary slightly from test.mx due to precision of solve()")
+message(" Note: Values vary slightly from test.mx due to precision of solve()")
 
 ##    Clean up objects created during test execution
-rm(test, test.functs, test.inv, test.inv2, test.mx)
+rm(test, test.functs, test.inv, test.inv2, test.mx, test.solve, test.mxret)
 ##    End of test routines.  Review the execution, and debug if necessary
